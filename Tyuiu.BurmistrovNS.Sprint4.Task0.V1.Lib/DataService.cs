e2a40c4327119589d4503ebc204f4d1daf865acb
Николай Бурмистров <Tyuiu.BurmistrovNS@outlook.com>
2025-11-12 19:37:16 +0500

    Add project files.

---
+++ Tyuiu.BurmistrovNS.Sprint4.Task0.V1.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.BurmistrovNS.Sprint4.Task0.V1.Lib
+{
+    public class DataService : ISprint4Task0V1
+    {
+        public int GetSumEvenArrEl(int[] array)
+        {
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    result += array[i];
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.BurmistrovNS.Sprint4.Task0.V1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.BurmistrovNS.Sprint4.Task0.V1.Lib;
+namespace Tyuiu.BurmistrovNS.Sprint4.Task0.V1
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("{6 ,4 ,3 ,2 ,1 ,0 ,9 ,8 ,7 ,5}");
+
+            int[] Array = new int[] { 6, 4, 3, 2, 1, 0, 9, 8, 7, 5 };
+
+            
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.GetSumEvenArrEl(Array);
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+
+
+
+
+}
