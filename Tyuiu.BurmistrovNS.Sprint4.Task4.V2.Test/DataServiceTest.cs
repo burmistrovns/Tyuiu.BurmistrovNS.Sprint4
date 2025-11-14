26cc86bffd650fc000543d14e525ecb27f0faa36
Николай Бурмистров <Tyuiu.BurmistrovNS@outlook.com>
2025-11-14 21:30:22 +0500

    Task4

---
+++ Tyuiu.BurmistrovNS.Sprint4.Task4.V2.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.BurmistrovNS.Sprint4.Task4.V2.Lib;
+
+namespace Tyuiu.BurmistrovNS.Sprint4.Task4.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[,] mas = new int[5, 5] { { 2, 2, 5, 5, 9 },
+                                         { 7, 3, 8, 8, 9 },
+                                         { 6, 7, 9, 3, 5 },
+                                         { 7, 5, 7, 7, 8 },
+                                         { 2, 2, 4, 7, 9 } };
+            int[,] res = ds.Calculate(mas);
+            int[,] wait = { { 2, 2, 0, 0, 0 },
+                             { 0, 0, 8, 8, 0 },
+                             { 6, 0, 0, 0, 0 },
+                             { 0, 0, 0, 0, 8 },
+                             { 2, 2, 4, 0, 0 } };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BurmistrovNS.Sprint4.Task4.V2/Program.cs
@@ -0,0 +1,70 @@
+﻿using Tyuiu.BurmistrovNS.Sprint4.Task4.V2.Lib;
+namespace Tyuiu.BurmistrovNS.Sprint4.Task4.V2
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов в массиве: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            int[,] mtrx = new int[rows, columns];
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"Введите {i}, {j} элемент массива: ");
+                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] res = ds.Calculate(mtrx);
+
+            Console.WriteLine("Новый массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{res[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+
+
+
+
+}
